85574a8531e83e1f9bb9daab7ecf987d67f08db5
Angel Abad <angelabad@gmail.com>
2020-04-20 18:33:34 +0200

    feat: Add descriptions on variables and outputs

--- outputs.tf
+++ outputs.tf
@@ -1,27 +1,34 @@
 output "default_security_group" {
-  value = aws_security_group.this.id
+  description = "Msk cluster default security group id."
+  value       = aws_security_group.this.id
 }
 
 output "arn" {
-  value = aws_msk_cluster.this.arn
+  description = "Amazon Resource Name (ARN) of the MSK cluster."
+  value       = aws_msk_cluster.this.arn
 }
 
 output "bootstrap_brokers" {
-  value = aws_msk_cluster.this.bootstrap_brokers
+  description = " A comma separated list of one or more hostname:port pairs of kafka brokers suitable to boostrap connectivity to the kafka cluster. Only contains value if client_broker encryption in transit is set o PLAINTEXT or TLS_PLAINTEXT."
+  value       = aws_msk_cluster.this.bootstrap_brokers
 }
 
 output "bootstrap_brokers_tls" {
-  value = aws_msk_cluster.this.bootstrap_brokers_tls
+  description = "A comma separated list of one or more DNS names (or IPs) and TLS port pairs kafka brokers suitable to boostrap connectivity to the kafka cluster. Only contains value if client_broker encryption in transit is set to TLS_PLAINTEXT or TLS."
+  value       = aws_msk_cluster.this.bootstrap_brokers_tls
 }
 
 output "current_version" {
-  value = aws_msk_cluster.this.current_version
+  description = "Current version of the MSK Cluster used for updates, e.g. K13V1IB3VIYZZH"
+  value       = aws_msk_cluster.this.current_version
 }
 
 output "encryption_at_rest_kms_key_arn" {
-  value = aws_msk_cluster.this.encryption_info.0.encryption_at_rest_kms_key_arn
+  description = "The ARN of the KMS key used for encryption at rest of the broker data volumes."
+  value       = aws_msk_cluster.this.encryption_info.0.encryption_at_rest_kms_key_arn
 }
 
 output "zookeeper_connect_string" {
-  value = aws_msk_cluster.this.zookeeper_connect_string
+  description = "A comma separated list of one or more hostname:port pairs to use to connect to the Apache Zookeeper cluster."
+  value       = aws_msk_cluster.this.zookeeper_connect_string
 }
--- variables.tf
+++ variables.tf
@@ -1,59 +1,72 @@
 variable "cluster_name" {
-  type = string
+  description = "Name of the MSK cluster."
+  type        = string
 }
 
 variable "kafka_version" {
-  type = string
+  description = "Specify the desired Kafka software version."
+  type        = string
 }
 
-variable "nodes" {
-  type = number
+variable "number_of_nodes" {
+  description = "The desired total number of broker nodes in the kafka cluster. It must be a multiple of the number of specified client subnets."
+  type        = number
 }
 
-variable "subnets" {
-  type = list(string)
+variable "client_subnets" {
+  description = "A list of subnets to connect to in client VPC"
+  type        = list(string)
 }
 
 variable "volume_size" {
-  type    = number
-  default = 1000
+  description = "The size in GiB of the EBS volume for the data drive on each broker node."
+  type        = number
+  default     = 1000
 }
 
 variable "instance_type" {
-  type = string
+  description = "Specify the instance type to use for the kafka brokers. e.g. kafka.m5.large."
+  type        = string
 }
 
 variable "extra_security_groups" {
-  type    = list(string)
-  default = []
+  description = "A list of extra security groups to associate with the elastic network interfaces to control who can communicate with the cluster."
+  type        = list(string)
+  default     = []
 }
 
 variable "prometheus_jmx_exporter" {
-  type    = bool
-  default = false
+  description = "Indicates whether you want to enable or disable the JMX Exporter."
+  type        = bool
+  default     = false
 }
 
 variable "prometheus_node_exporter" {
-  type    = bool
-  default = false
+  description = "Indicates whether you want to enable or disable the Node Exporter."
+  type        = bool
+  default     = false
 }
 
 variable "server_properties" {
-  type    = map(string)
-  default = {}
+  description = "A map of the contents of the server.properties file. Supported properties are documented in the [MSK Developer Guide](https://docs.aws.amazon.com/msk/latest/developerguide/msk-configuration-properties.html)."
+  type        = map(string)
+  default     = {}
 }
 
 variable "encryption_at_rest_kms_key_arn" {
-  type    = string
-  default = null
+  description = "You may specify a KMS key short ID or ARN (it will always output an ARN) to use for encrypting your data at rest. If no key is specified, an AWS managed KMS ('aws/msk' managed service) key will be used for encrypting the data at rest."
+  type        = string
+  default     = null
 }
 
 variable "encryption_in_transit_client_broker" {
-  type    = string
-  default = "TLS"
+  description = "Encryption setting for data in transit between clients and brokers. Valid values: TLS, TLS_PLAINTEXT, and PLAINTEXT. Default value is TLS_PLAINTEXT when encryption_in_transit block defined, but TLS when encryption_in_transit block omitted."
+  type        = string
+  default     = "TLS"
 }
 
 variable "encryption_in_transit_in_cluster" {
-  type    = bool
-  default = true
+  description = "Whether data communication among broker nodes is encrypted. Default value: true."
+  type        = bool
+  default     = true
 }
